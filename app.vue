db6ef699231ac6b24590a07dca2500c503a6b6ea
Abhinash Khatiwada <me@abhinash.net>
2024-02-10 16:40:01 +0545

    feat: add loading indicator

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <NuxtLayout>
+    <NuxtLoadingIndicator
+      :color="false"
+      class="bg-gradient-to-l from-primary-700 to-primary-500"
+    ></NuxtLoadingIndicator>
     <NuxtPage />
     <UNotifications />
   </NuxtLayout>
