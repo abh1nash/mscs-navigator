b369fd6646a1ea1e0d92dbc71e2c4616f5165abe
Abhinash Khatiwada <me@abhinash.net>
2024-02-11 17:56:06 +0545

    fix: investigate point of failure

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-  <AppMainHeader class="fixed top-0"></AppMainHeader>
-  <div class="flex-1">
-    <slot></slot>
+  <div>
+    <AppMainHeader class="fixed top-0"></AppMainHeader>
+    <div class="flex-1">
+      <slot></slot>
+    </div>
+    <div class="h-10 w-full bg-primary"></div>
   </div>
-  <div class="h-10 w-full bg-primary"></div>
 </template>
