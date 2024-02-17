447459d063189ff78fe65ef06e9b53bd8a4a2608
Abhinash Khatiwada <me@abhinash.net>
2024-02-17 19:51:44 +0545

    ci: use custom error page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@vue-email/nuxt", "@vueuse/nuxt", "nuxt-rating"],
-  build: {
-    transpile: ["nuxt-rating"],
-  },
   appConfig: {
     plunkAPIKey: process.env.PLUNK_API_KEY,
   },
