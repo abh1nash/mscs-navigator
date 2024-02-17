42f939b87033680c6629214d5a229a22c5b96fac
Abhinash Khatiwada <me@abhinash.net>
2024-02-17 20:49:08 +0545

    fix: make use of custom import

--- server/api/auth/reset-password/index.post.ts
+++ server/api/auth/reset-password/index.post.ts
@@ -2,6 +2,7 @@ import { db } from "~/server/data/prisma";
 import * as resetToken from "~/server/data/reset-token";
 import * as mailer from "~/server/utils/mailer";
 import { useCompiler } from "#vue-email";
+import template from "~/emails/ResetPassword.vue";
 
 const success = {
   message:
@@ -24,7 +25,7 @@ export default defineEventHandler(async (event) => {
 
   const token = await resetToken.create(user.id);
 
-  const template = await useCompiler("ResetPassword.vue", {
+  const template = await useCompiler(template, {
     props: { link: getHeader(event, "Host") + "/auth/reset-password/" + token },
   });
 
